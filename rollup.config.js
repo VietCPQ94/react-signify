a5d577ceecad8f45e527381efa25ef957e4aecca
Quốc Việt Công Phạm <congphamquocviet@gmail.com>
2024-05-21 02:58:52 +0700

    hotfix: Update config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,46 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.tsx",
+    input: 'src/index.tsx',
     output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
+      // {
+      //   file: packageJson.main,
+      //   format: 'cjs',
+      //   sourcemap: true
+      // },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
+        format: 'esm',
+        sourcemap: true
+      }
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: './tsconfig.json' }),
       terser(),
       postcss(),
+      copy({
+        targets: [{ src: 'package.json', dest: 'dist' }]
+      })
     ],
-    external: ["react", "react-dom"],
+    external: ['react', 'react-dom']
   },
   {
-    input: "src/index.tsx",
-    output: [{ file: "dist/types.d.ts", format: "cjs" }],
+    input: 'src/index.tsx',
+    output: [{ file: packageJson.types, format: 'cjs' }],
     plugins: [dts.default()],
-    external: [/\.css$/],
-  },
+    external: [/\.css$/]
+  }
 ];
