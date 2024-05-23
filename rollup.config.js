8bf37510f1616d77e8273ab08d8c188d953fe315
Quốc Việt Công Phạm <congphamquocviet@gmail.com>
2024-05-23 22:53:19 +0700

    Fix: optimize source code

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
 
 const packageJson = require('./package.json');
 
@@ -18,7 +20,28 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser(), postcss()],
+    globals: {
+      react: 'React'
+    },
+    resolve: () => {},
+    plugins: [
+      alias({
+        entries: [
+          { find: 'react', replacement: path.resolve(__dirname, 'node_modules/react') },
+          { find: 'react-dom', replacement: path.resolve(__dirname, 'node_modules/react-dom') }
+        ]
+      }),
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx'] // Các phần mở rộng mà Rollup sẽ xử lý
+      }),
+      commonjs({
+        include: /node_modules/
+      }),
+      peerDepsExternal(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+      postcss()
+    ],
     external: ['react', 'react-dom']
   },
   {
