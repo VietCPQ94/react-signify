847da79f8958c95683e37e0883a7a6cbe1db6a8c
VietCPQ <congphamquocviet@gmail.com>
2025-08-14 16:25:49 +0700

    opt: code of clone and compare

--- src/package/utils/objectClone.ts
+++ src/package/utils/objectClone.ts
@@ -1,15 +1,3 @@
-// Function to create a copy of a Buffer or ArrayBuffer view
-const copyBuffer = (cur: any) => {
-    // Check if the current object is an instance of Buffer
-    if (cur instanceof Buffer) {
-        // Return a new Buffer from the current one
-        return Buffer.from(cur);
-    }
-
-    // For other ArrayBuffer views, create a new instance using the constructor and provide the buffer slice
-    return new cur.constructor(cur.buffer.slice(), cur.byteOffset, cur.length);
-};
-
 /**
  * Deep clone any JavaScript object with support for circular references and custom constructors
  * @param source The object to be cloned
@@ -19,7 +7,7 @@ const copyBuffer = (cur: any) => {
 export const deepClone = <T>(
     source: T,
     options?: {
-        constructorHandlers?: [Function, (obj: any, clone: (o: any) => any) => any][]
+        constructorHandlers?: [Function, (obj: any, fn?: (o: any) => any) => any][];
     }
 ): T => {
     // Handle primitives, null, and undefined directly
@@ -30,116 +18,51 @@ export const deepClone = <T>(
     // Create a WeakMap to track already cloned objects (for circular references)
     const cloneMap = new WeakMap();
 
-    // Map to hold custom constructors and their cloning functions
-    const constructorHandlers = new Map();
-    
-    // Add default handlers for common built-in objects
-    constructorHandlers.set(Date, (obj: Date) => new Date(obj));
-    constructorHandlers.set(Map, (obj: Map<any, any>, fn: (o: any) => any) => 
-        new Map(Array.from(obj.entries()).map(([key, val]) => [fn(key), fn(val)])));
-    constructorHandlers.set(Set, (obj: Set<any>, fn: (o: any) => any) => 
-        new Set(Array.from(obj).map(val => fn(val))));
-    constructorHandlers.set(RegExp, (obj: RegExp) => new RegExp(obj.source, obj.flags));
-    constructorHandlers.set(Error, (obj: Error, fn: (o: any) => any) => {
-        const error = new Error(obj.message);
-        if (obj.stack) error.stack = obj.stack;
-        if (obj.cause) error.cause = fn(obj.cause);
-        return error;
-    });
-    constructorHandlers.set(URL, (obj: URL) => new URL(obj.toString()));
-    constructorHandlers.set(Blob, (obj: Blob) => obj.slice(0, obj.size, obj.type));
-    constructorHandlers.set(File, (obj: File) => new File([obj.slice(0, obj.size)], obj.name, { 
-        type: obj.type, 
-        lastModified: obj.lastModified 
-    }));
-    constructorHandlers.set(FileList, (obj: FileList, fn: (o: any) => any) => {
-        // FileList can't be directly constructed, so we clone each File
-        const files = Array.from(obj).map(file => fn(file));
-        // Return the array of files since we can't create a proper FileList
-        return Object.defineProperty(files, 'item', {
-            value: (index: number) => files[index],
-            enumerable: false
-        });
-    });
-    
-    // If additional constructor handlers are provided in options, add them to the map
-    if (options?.constructorHandlers) {
-        for (const handler of options.constructorHandlers) {
-            constructorHandlers.set(handler[0], handler[1]);
-        }
-    }
+    // Built-in object handlers
+    const handlers = new Map<Function, (obj: any, fn?: (o: any) => any) => any>([
+        [Date, (obj: Date) => new Date(obj.getTime())],
+        [RegExp, (obj: RegExp) => new RegExp(obj.source, obj.flags)],
+        [Map, (obj: Map<any, any>, fn?: (o: any) => any) => new Map([...obj].map(([k, v]) => [fn!(k), fn!(v)]))],
+        [Set, (obj: Set<any>, fn?: (o: any) => any) => new Set([...obj].map(fn!))],
+        [Error, (obj: Error, fn?: (o: any) => any) => Object.assign(new (obj.constructor as any)(obj.message), { stack: obj.stack, cause: obj.cause && fn!(obj.cause) })],
+        [URL, (obj: URL) => new URL(obj.href)],
+        [Blob, (obj: Blob) => obj.slice()],
+        [File, (obj: File) => new File([obj], obj.name, { type: obj.type, lastModified: obj.lastModified })],
+        ...(options?.constructorHandlers || [])
+    ]);
 
-    // The main clone function
+    // Main clone function
     const clone = (obj: any): any => {
-        // Handle primitives, null, and undefined directly
-        if (typeof obj !== 'object' || obj === null) {
-            return obj;
-        }
-        
-        // Check if this object has already been cloned (circular reference)
-        if (cloneMap.has(obj)) {
-            return cloneMap.get(obj);
-        }
-        
-        // Handle arrays
+        // Handle primitives
+        if (typeof obj !== 'object' || obj === null) return obj;
+
+        // Handle circular references
+        if (cloneMap.has(obj)) return cloneMap.get(obj);
+
+        let result: any;
+
+        // Handle different object types
         if (Array.isArray(obj)) {
-            const result: any[] = [];
+            result = obj.map(clone);
+        } else if (ArrayBuffer.isView(obj)) {
+            result = obj instanceof Buffer ? Buffer.from(obj) : new (obj.constructor as any)(obj.buffer.slice(), obj.byteOffset, (obj as any).length);
+        } else if (handlers.has(obj.constructor)) {
+            result = handlers.get(obj.constructor)!(obj, clone);
+        } else {
+            // Handle plain objects
+            result = Object.create(Object.getPrototypeOf(obj));
             cloneMap.set(obj, result);
-            
-            for (let i = 0; i < obj.length; i++) {
-                result[i] = clone(obj[i]);
-            }
-            
+
+            // Copy all properties in one pass
+            [...Object.getOwnPropertyNames(obj), ...Object.getOwnPropertySymbols(obj)].forEach(key => {
+                const descriptor = Object.getOwnPropertyDescriptor(obj, key)!;
+                if (descriptor.value !== undefined) descriptor.value = clone(descriptor.value);
+                Object.defineProperty(result, key, descriptor);
+            });
             return result;
         }
-        
-        // Handle ArrayBuffer views
-        if (ArrayBuffer.isView(obj)) {
-            return copyBuffer(obj);
-        }
-        
-        // Handle custom constructor objects
-        if (obj.constructor !== Object && constructorHandlers.has(obj.constructor)) {
-            const result = constructorHandlers.get(obj.constructor)(obj, clone);
-            if (typeof result === 'object' && result !== null) {
-                cloneMap.set(obj, result);
-            }
-            return result;
-        }
-        
-        // Create a new instance with the same prototype for regular objects
-        const result = Object.create(Object.getPrototypeOf(obj));
+
         cloneMap.set(obj, result);
-        
-        // Copy all enumerable properties
-        for (const key of Object.keys(obj)) {
-            result[key] = clone(obj[key]);
-        }
-        
-        // Copy non-enumerable properties that are directly on the object
-        for (const key of Object.getOwnPropertyNames(obj)) {
-            if (!Object.prototype.propertyIsEnumerable.call(obj, key)) {
-                const descriptor = Object.getOwnPropertyDescriptor(obj, key);
-                if (descriptor) {
-                    if (descriptor.value !== undefined) {
-                        descriptor.value = clone(descriptor.value);
-                    }
-                    Object.defineProperty(result, key, descriptor);
-                }
-            }
-        }
-        
-        // Copy symbol properties
-        for (const sym of Object.getOwnPropertySymbols(obj)) {
-            const descriptor = Object.getOwnPropertyDescriptor(obj, sym);
-            if (descriptor) {
-                if (descriptor.value !== undefined) {
-                    descriptor.value = clone(descriptor.value);
-                }
-                Object.defineProperty(result, sym, descriptor);
-            }
-        }
-        
         return result;
     };
 
--- src/package/utils/objectCompare.ts
+++ src/package/utils/objectCompare.ts
@@ -1,52 +1,24 @@
-// Function to check if two values are equal
-export const deepCompare = (a: any, b: any) => {
-    // If both values are strictly equal, return true
+export const deepCompare = (a: any, b: any): boolean => {
     if (a === b) return true;
+    if (!a || !b || typeof a !== 'object' || typeof b !== 'object') return a !== a && b !== b; // NaN check
 
-    // Check if both values are objects
-    if (a && b && typeof a == 'object' && typeof b == 'object') {
-        // If the constructors of both objects are different, return false
-        if (a.constructor !== b.constructor) return false;
+    if (a.constructor !== b.constructor) return false;
 
-        var length, i, keys;
-
-        // Check if both values are arrays
-        if (Array.isArray(a)) {
-            length = a.length;
-            // If the lengths of the arrays are different, return false
-            if (length != b.length) return false;
-            // Recursively check each element in the arrays
-            for (i = length; i-- !== 0; ) if (!deepCompare(a[i], b[i])) return false;
-            return true;
-        }
-
-        // Check for RegExp equality
-        if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
-
-        // Check for valueOf method equality
-        if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
-
-        // Check for toString method equality
-        if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
-
-        // Get the keys of the first object
-        keys = Object.keys(a);
-        length = keys.length;
-        // If the number of keys in both objects is different, return false
-        if (length !== Object.keys(b).length) return false;
-
-        // Check that all keys in the first object exist in the second object
-        for (i = length; i-- !== 0; ) if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
-
-        // Recursively check each key-value pair for equality
-        for (i = length; i-- !== 0; ) {
-            var key = keys[i];
-            if (!deepCompare(a[key], b[key])) return false;
-        }
-
-        return true; // All checks passed, the objects are equal
+    // Array comparison
+    if (Array.isArray(a)) {
+        if (a.length !== b.length) return false;
+        for (let i = a.length; i--; ) if (!deepCompare(a[i], b[i])) return false;
+        return true;
     }
 
-    // Check for NaN equality (both values are NaN)
-    return a !== a && b !== b;
+    // Special object types
+    if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
+    if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
+    if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
+
+    // Object comparison
+    const keys = Object.keys(a);
+    if (keys.length !== Object.keys(b).length) return false;
+
+    return keys.every(key => Object.prototype.hasOwnProperty.call(b, key) && deepCompare(a[key], b[key]));
 };
